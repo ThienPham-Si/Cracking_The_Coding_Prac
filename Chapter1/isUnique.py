1e0fb64de06e331b02664f09ba9134bc4dc5d279
Gordonfreeman1564 <thienpham.3dev@gmail.com>
2021-04-14 13:11:40 -0500

    Create isUnique.py

---
+++ Chapter1/isUnique.py
@@ -0,0 +1,17 @@
+import collections
+
+def isUnique(str):
+    letters_dict = collections.defaultdict(int)
+    for characters in str:
+        letters_dict[characters] += 1
+
+    print(letters_dict)
+    for key, value in letters_dict.items():
+        if value > 1:
+            return False
+
+    return True
+
+if __name__ == "__main__":
+    print(isUnique('thisissparta'))
+    print(isUnique('Unique'))
